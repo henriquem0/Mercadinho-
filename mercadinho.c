9623ffb1770dd4e873f215cb271cb2ecebc784e7
Henrique <henrique.mohr@usp.br>
2026-06-24 16:41:22 -0300

    Update mercadinho.c

--- mercadinho.c
+++ mercadinho.c
@@ -1,7 +1,9 @@
+//Sistema de gerenciamento de estoque e caixa para um pequeno mercado, desenvolvido como projeto para a disciplina de Introdução à Ciência da Computação (ICC).
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*definição da struct produto*/
 
@@ -12,38 +14,40 @@ typedef struct
   double preco;
 } produto;
 
-///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-int qualC(char C[3]);                         /*leitura de comando e conversão para um int*/
-void ip(produto *P);                          /*incremento de novo produto ao estoque*/
-double ae(produto *P, int Q);                 /*aumento de estoque em Q do produto P*/
+int qualC(char C[3]);                         /*leitura de comando e conversão para um int, usando como parâmetro uma string para leitura dos 2 dígitos do comando*/
+void ip(produto *P);                          /*incremento de novo produto ao estoque, usando como parâmetro um ponteiro de produto, o qual referenciará o produto a ser adicionado, com variáveis *nome, qtd e preco, partes da struct*/
+double ae(produto *P, int Q);                 /*aumento de estoque em Q do produto P, que retorna o valor a ser descontado do saldo na main*/
 void mp(produto *P, double p);                /*alteração do preço do produto P para p*/
-double ve(produto *P);                        /*venda*/
-void ce(produto *P, int N);                   /*consultar estoque de todos os produtos*/
-void cs(double s);                            /*consultar saldo*/
-void alocar(int *N, int Ptotal, produto **P); /*alocação de memória caso o número de produto exceda o estoque*/
+double ve(produto *P);                        /*venda, que retorna o valor a ser somado ao saldo na main*/
+void ce(produto *P, int N);                   /*consultar estoque de todos os N produtos*/
+void cs(double s);                            /*consultar saldo, com o saldo de parâmetro único*/
+void alocar(int *N, int Ptotal, produto **P); /*alocação de memória caso o número de produtos Ptotal exceda o estoque N*/
 
-///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void)
 {
-  /*declaração de variáveis*/
-  int N, Ptotal = 0, Q, i;
-  double p, saldo;
-  char comando[3];
+
+  int N, Ptotal = 0, Q, i; /*N é o tamanho do estoque de produtos, Ptotal é o total de produtos cadastrados e Q e i são variáveis auxiliares para leitura de quantidade e índice, respectivamente, quando necessário em comandos*/
+  double p, saldo; /*saldo é referente ao total acumulado em caixa e p é uma variável auxiliar para a leitura de preço, quando necessário em comandos*/
+  char comando[3]; /*string dedicada para a leitura do comando, sobretudo no laço while e na chamada à função qualC*/
   FILE *fp;
-  produto *P;
+  produto *P; /*ponteiro de struct produtos para armazenamento dos produtos em índices*/
 
   fp = fopen("estoque.txt", "r");
 
+	/*processo de análise de existência de arquivo anterior para inicialização. Em caso negativo, o programa inicializa o saldo e o N, este de acordo com a entrada dada*/
+
   if (fp == NULL)
-  { /*inicializa as variáveis saldo e N caso não haja arquivo pré-existente*/
+  {
     saldo = 100;
     scanf("%d", &N);
-    P = calloc(N, sizeof(produto));
+    P = calloc(N, sizeof(produto)); /*alocação dinâmica de estoque inicial N de produtos*/
   }
   else
-  { /*carrega o arquivo pré existente*/
+  {
     fscanf(fp, "%d %d %lf", &N, &Ptotal, &saldo);
     P = calloc(N, sizeof(produto));
     for (i = 0; i < Ptotal; i++)
@@ -57,6 +61,8 @@ int main(void)
     fclose(fp);
   }
 
+	/*leitura sequencial de comandos até que haja leitura do comando FE (finalizar dia)*/
+
   scanf(" %2s", comando);
   while (strcmp(comando, "FE") != 0)
   {
@@ -64,32 +70,32 @@ int main(void)
     /*definição da função a partir do int devolvido por qualC*/
     switch (qualC(comando))
     {
-    case 1:
+    case 1: /*IP*/
     {
-      Ptotal++;
-      alocar(&N, Ptotal, &P);
-      ip(&P[Ptotal - 1]);
+      Ptotal++; /*incremento do número de produtos cadastrados*/
+      alocar(&N, Ptotal, &P); 
+      ip(&P[Ptotal - 1]); /*cadastro do novo produto e suas variáveis utilizando o último indice adicionado*/
       break;
     }
-    case 2:
+    case 2: /*AE*/
       scanf("%d%d", &i, &Q);
       saldo -= ae(&P[i], Q);
       break;
-    case 3:
+    case 3: /*MP*/
       scanf("%d%lf", &i, &p);
       mp(&P[i], p);
       break;
-    case 4:
+    case 4: /*VE*/
       saldo += ve(P);
       break;
-    case 5:
+    case 5: /*CE*/
       ce(P, Ptotal);
       break;
-    case 6:
+    case 6: /*CS*/
       cs(saldo);
       break;
     }
-    scanf(" %2s", comando);
+    scanf(" %2s", comando); /*leitura do próximo comando para comparar com FE para o fechamento do loop*/
   }
 
   /*salva arquivo e libera memória para encerramento do programa*/
@@ -114,7 +120,7 @@ int main(void)
 
   return 0;
 }
-///////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int qualC(char C[3])
 {
@@ -135,7 +141,7 @@ int qualC(char C[3])
 
 void ip(produto *P)
 {
-  char buffer[100];
+  char buffer[100]; /*string auxiliar para leitura do nome do produto a ser adicionado para alocar dinâmicamente a partir de strlen(buffer)*/
   scanf("%99s", buffer);
 
   int tam = strlen(buffer);
@@ -150,7 +156,7 @@ double ae(produto *P, int Q)
 {
   double s;
   (*P).qtd += Q;
-  s = ((*P).preco * Q);
+  s = ((*P).preco * Q); /*cálculo do custo para aumentar estoque levando em conta o preço do produto. Este valor é retornado e subtraído do saldo na int main*/
   return s;
 }
 
@@ -162,16 +168,16 @@ void mp(produto *P, double p)
 double ve(produto *P)
 {
   int i;
-  double total = 0;
-  scanf("%d", &i);
+  double total = 0; /*valor total da compra*/
+  scanf("%d", &i); /* leitura do primeiro índice dado na linha do comando venda*/
 
-  while (i != -1)
+  while (i != -1) /*laço de repetição até que o valor i lido seja -1, que fecha o comando de venda*/
   {
     if (P[i].qtd > 0)
     { /*só vende se ainda houver unidade em estoque*/
       printf("%s %.2lf\n", P[i].nome, P[i].preco);
       total += P[i].preco;
-      P[i].qtd -= 1;
+      P[i].qtd -= 1;/*redução em 1 do estoque do produto de índice i*/
     }
     scanf("%d", &i);
   }
